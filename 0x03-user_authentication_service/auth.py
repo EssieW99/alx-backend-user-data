547a1d282ed70f0d5645f46aa070af460f8002ae
Esther Wanjiku <esther.wanjikum99@gmail.com>
2024-09-14 22:25:27 +0300

    5. Register user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -14,3 +14,25 @@ def _hash_password(password: str) -> bytes:
     hashed_pwd = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
     return hashed_pwd
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        registers a user
+        """
+
+        db = self._db
+
+        user = db.find_user_by(email=email)
+        if user:
+            raise ValueError(f"User {user.email} already exists")
+
+        hashed_password = _hash_password(password)
+        db.add_user(email, hashed_password)
